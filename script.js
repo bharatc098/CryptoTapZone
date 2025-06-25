d19f3cba4c43bf75f590f1f6b3c8a82fc78c7cb6
bharatc098 <bhart07677@gmail.com>
2025-06-26 01:40:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+  new TradingView.widget({
+    autosize: true,
+    symbol: "BINANCE:BTCUSDT",
+    interval: "3",
+    timezone: "Asia/Kolkata",
+    theme: "dark",
+    style: "1",
+    locale: "en",
+    toolbar_bg: "#000000",
+    enable_publishing: false,
+    hide_top_toolbar: false,
+    hide_legend: false,
+    allow_symbol_change: true,
+    container_id: "tv-chart"
+  });
+});
