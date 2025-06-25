6f16ebdc22437b261751df73a9db3d6de7b076ee
bharatc098 <bhart07677@gmail.com>
2025-06-26 01:46:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-  new TradingView.widget({
-    autosize: true,
-    symbol: "BINANCE:BTCUSDT",
-    interval: "3",
-    timezone: "Asia/Kolkata",
-    theme: "dark",
-    style: "1",
-    locale: "en",
-    toolbar_bg: "#000000",
-    enable_publishing: false,
-    hide_top_toolbar: false,
-    hide_legend: false,
-    allow_symbol_change: true,
-    container_id: "tv-chart"
-  });
+new TradingView.widget({
+  container_id: "tv-chart",
+  autosize: true,
+  symbol: "NSE:NIFTY",
+  interval: "3",
+  timezone: "Asia/Kolkata",
+  theme: "dark",
+  style: "1",
+  locale: "en",
+  toolbar_bg: "#1e1e1e",
+  enable_publishing: false,
+  allow_symbol_change: true,
+  hide_legend: false,
+  studies: [
+    "ADX@tv-basicstudies",          // ADX default
+    "MAExp@tv-basicstudies",        // EMA (आपण नंतर 22 सेट करू)
+  ],
 });
